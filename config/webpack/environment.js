7ff493a1918400b7e0dbea79095a8803e639d035
Kentaro JOKO <kentaro_joko@acceluniverse.com>
2025-10-24 14:32:46 +0900

    顧客情報の自宅住所と勤務先は任意登録とするよう修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker")
+
+const webpack = require("webpack")
+environment.plugins.prepend("Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQery: "jquery/srv/jquery"
+  })
+)
 
 module.exports = environment
